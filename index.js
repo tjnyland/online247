1b50d8d5ce62648f6fa9b770cedbac00bf2f660c
TJGitHub-Coder <56506419+TJGitHub-Coder@users.noreply.github.com>
2020-09-27 12:43:43 +0100

    idk

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const {Client, Attachment, MessageEmbed} = require('discord.js');
 
-
-
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
